ad5cf9e14597b61f9d3e77c6b8dab615ff7e2f8e
Nguyen Phung Hai <33575709+phunghai@users.noreply.github.com>
2018-03-18 10:58:48 +0700

    Add files via upload

--- source_python/app.py
+++ source_python/app.py
@@ -27,19 +27,67 @@ def invite():
         board_height = Constant.DEFAULT_BOARD_HEIGHT
 
     ships = request.json["ships"]
+
     print(type(ships), ships)
-
     # store init game informations to session
-    session["board_width"] = board_width
+    session[Constant.SESSION_KEY_BOARD_WIDTH] = board_width
+    session[Constant.SESSION_KEY_BOARD_HEIGHT] = board_height
+    session[Constant.SESSION_KEY_INVITE_SHIPS] = ships
 
-    return jsonify({"boardWidth": board_width})
+    return jsonify({"message": "successful"})
 
 @app.route("/place-ships", methods=["POST"])
 def place_ships():
-    board_width = session["board_width"]
-    print(type(board_width), board_width)
+    # get information from request
+    player1 = request.json["player1"]
+    player2 = request.json["player2"]
 
-    return "ok"
+    # get information from session
+    board_width = session[Constant.SESSION_KEY_BOARD_WIDTH]
+    board_height = session[Constant.SESSION_KEY_BOARD_HEIGHT]
+    ships = session[Constant.SESSION_KEY_INVITE_SHIPS]
+    print(type(ships), ships)
+    ret_ships = []
+    for ship in ships:
+        for quantity in range(ship["quantity"]):
+            ret_ship = {}
+            ret_ship["type"] = ship["type"]
+            ret_ship["coordinates"] = [[1,2], [2,1]]
+            ret_ships.append(ret_ship)
+    #TODO
+
+    return jsonify(ret_ships)
+
+@app.route("/shot", methods=["POST"])
+def shot():
+    # counter for turn in session
+    turn = request.json["turn"]
+
+    max_shots = request.json["maxShots"]
+
+    if max_shots > 1:
+        print("Combo")
+
+    coordinates = []
+    for shot in range(max_shots):
+        coordinate = [1,2]
+        coordinates.append(coordinate)
+    return jsonify({"coordinates" : coordinates})
+
+@app.route("/notify", methods=["POST"])
+def notify():
+    player_id = request.json["playerId"]
+    shots = request.json["shots"]
+    # analysis status shot
+    for shot in shots:
+        print(shot["coordinate"], shot["status"])
+
+    sunk_ships = request.json["sunkShips"]
+    # analysis sunk ship
+    for sunk_ship in sunk_ships:
+        print(sunk_ship["type"], sunk_ship["coordinates"])
+
+    return jsonify({"message": "successful"})
 
 if __name__ == '__main__':
     app.run(debug=True)
